978945499eb6614749ca49049099259ae90b326d
keerthikrishnap-droid <keerthikrishnap2005@gmail.com>
2026-07-13 13:37:43 +0530

    Update 2_Forecast_Explorer.py

--- pages/2_Forecast_Explorer.py
+++ pages/2_Forecast_Explorer.py
@@ -1,181 +1,181 @@
 
-# Forecast Explorer
+# # Forecast Explorer
 
 
-import streamlit as st
-import pandas as pd
-import matplotlib.pyplot as plt
-from prophet import Prophet
+# import streamlit as st
+# import pandas as pd
+# import matplotlib.pyplot as plt
+# from prophet import Prophet
 
 
 
 
-st.set_page_config(
-    page_title="Forecast Explorer",
-    layout="wide"
-)
+# st.set_page_config(
+#     page_title="Forecast Explorer",
+#     layout="wide"
+# )
 
-st.title("Forecast Explorer")
-
-
-# Load Dataset
+# st.title("Forecast Explorer")
 
 
-@st.cache_data
-def load_data():
-    df = pd.read_csv("train.csv")
-
-    df["Order Date"] = pd.to_datetime(
-        df["Order Date"],
-        format="%d/%m/%Y"
-    )
-
-    return df
-
-df = load_data()
+# # Load Dataset
 
 
-# User Inputs
+# @st.cache_data
+# def load_data():
+#     df = pd.read_csv("train.csv")
+
+#     df["Order Date"] = pd.to_datetime(
+#         df["Order Date"],
+#         format="%d/%m/%Y"
+#     )
+
+#     return df
+
+# df = load_data()
 
 
-forecast_type = st.selectbox(
-    "Forecast Type",
-    ["Category", "Region"]
-)
-
-if forecast_type == "Category":
-    selected = st.selectbox(
-        "Select Category",
-        sorted(df["Category"].unique())
-    )
-
-    filtered_df = df[df["Category"] == selected]
-
-else:
-    selected = st.selectbox(
-        "Select Region",
-        sorted(df["Region"].unique())
-    )
-
-    filtered_df = df[df["Region"] == selected]
-
-forecast_horizon = st.slider(
-    "Forecast Horizon (Months)",
-    min_value=1,
-    max_value=3,
-    value=3
-)
+# # User Inputs
 
 
-# Prepare Monthly Sales
+# forecast_type = st.selectbox(
+#     "Forecast Type",
+#     ["Category", "Region"]
+# )
+
+# if forecast_type == "Category":
+#     selected = st.selectbox(
+#         "Select Category",
+#         sorted(df["Category"].unique())
+#     )
+
+#     filtered_df = df[df["Category"] == selected]
+
+# else:
+#     selected = st.selectbox(
+#         "Select Region",
+#         sorted(df["Region"].unique())
+#     )
+
+#     filtered_df = df[df["Region"] == selected]
+
+# forecast_horizon = st.slider(
+#     "Forecast Horizon (Months)",
+#     min_value=1,
+#     max_value=3,
+#     value=3
+# )
 
 
-monthly = (
-    filtered_df
-    .groupby(
-        pd.Grouper(
-            key="Order Date",
-            freq="ME"
-        )
-    )["Sales"]
-    .sum()
-    .reset_index()
-)
-
-monthly.columns = ["ds", "y"]
+# # Prepare Monthly Sales
 
 
-# Train Prophet
+# monthly = (
+#     filtered_df
+#     .groupby(
+#         pd.Grouper(
+#             key="Order Date",
+#             freq="ME"
+#         )
+#     )["Sales"]
+#     .sum()
+#     .reset_index()
+# )
+
+# monthly.columns = ["ds", "y"]
 
 
-model = Prophet(
-    yearly_seasonality=True,
-    weekly_seasonality=False,
-    daily_seasonality=False
-)
-
-model.fit(monthly)
-
-future = model.make_future_dataframe(
-    periods=forecast_horizon,
-    freq="ME"
-)
-
-forecast = model.predict(future)
+# # Train Prophet
 
 
-# Plot Forecast
+# model = Prophet(
+#     yearly_seasonality=True,
+#     weekly_seasonality=False,
+#     daily_seasonality=False
+# )
+
+# model.fit(monthly)
+
+# future = model.make_future_dataframe(
+#     periods=forecast_horizon,
+#     freq="ME"
+# )
+
+# forecast = model.predict(future)
 
 
-st.subheader("Forecast")
-
-fig = model.plot(forecast)
-
-st.pyplot(fig)
+# # Plot Forecast
 
 
-# Forecast Table
+# st.subheader("Forecast")
+
+# fig = model.plot(forecast)
+
+# st.pyplot(fig)
 
 
-st.subheader("Forecast Values")
-
-future_values = forecast.tail(forecast_horizon)[
-    [
-        "ds",
-        "yhat",
-        "yhat_lower",
-        "yhat_upper"
-    ]
-]
-
-future_values.columns = [
-    "Month",
-    "Forecast",
-    "Lower CI",
-    "Upper CI"
-]
-
-st.dataframe(future_values)
+# # Forecast Table
 
 
-# Model Performance
+# st.subheader("Forecast Values")
+
+# future_values = forecast.tail(forecast_horizon)[
+#     [
+#         "ds",
+#         "yhat",
+#         "yhat_lower",
+#         "yhat_upper"
+#     ]
+# ]
+
+# future_values.columns = [
+#     "Month",
+#     "Forecast",
+#     "Lower CI",
+#     "Upper CI"
+# ]
+
+# st.dataframe(future_values)
 
 
-st.subheader("Model Performance")
+# # Model Performance
 
-# st.write("Model Used: Prophet")
 
-# st.write("MAE : 14309.99")
+# st.subheader("Model Performance")
 
-# st.write("RMSE : 18954.58")
-metrics = pd.read_csv(
-    "forecasts/forecast_metrics.csv"
-)
+# # st.write("Model Used: Prophet")
 
-metric1, metric2, metric3 = st.columns(3)
+# # st.write("MAE : 14309.99")
 
-with metric1:
-    st.metric(
-        "Model",
-        metrics.loc[0, "Model"]
-    )
+# # st.write("RMSE : 18954.58")
+# metrics = pd.read_csv(
+#     "forecasts/forecast_metrics.csv"
+# )
 
-with metric2:
-    st.metric(
-        "MAE",
-        f"{metrics.loc[0, 'MAE']:,.2f}"
-    )
+# metric1, metric2, metric3 = st.columns(3)
 
-with metric3:
-    st.metric(
-        "RMSE",
-        f"{metrics.loc[0, 'RMSE']:,.2f}"
-    )
+# with metric1:
+#     st.metric(
+#         "Model",
+#         metrics.loc[0, "Model"]
+#     )
 
-# col1, col2 = st.columns(2)
+# with metric2:
+#     st.metric(
+#         "MAE",
+#         f"{metrics.loc[0, 'MAE']:,.2f}"
+#     )
 
-# with col1:
-#     st.metric("MAE", "14,309.99")
+# with metric3:
+#     st.metric(
+#         "RMSE",
+#         f"{metrics.loc[0, 'RMSE']:,.2f}"
+#     )
 
-# with col2:
-#     st.metric("RMSE", "18,954.58")
+# # col1, col2 = st.columns(2)
+
+# # with col1:
+# #     st.metric("MAE", "14,309.99")
+
+# # with col2:
+# #     st.metric("RMSE", "18,954.58")
